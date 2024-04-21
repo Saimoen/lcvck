3b614d3a850def3d8e7fbe935a4571e60a8e9a5d
Saimoen <gregorysaimoen@outlook.com>
2024-04-22 09:06:47 +1100

    ajout des images

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [LeafletModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 30,
+        attribution: '...',
+      }),
+    ],
+    zoom: 7,
+    center: latLng(-22.272453946632073, 166.43851442916946),
+  };
 }
