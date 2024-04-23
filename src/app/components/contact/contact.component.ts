2dc0f86cdbf7410a7b75927af61b5f4b12f0f7aa
Saimoen <gregorysaimoen@outlook.com>
2024-04-23 13:54:52 +1100

    ajout d'un marqueur sur leaflet + responsive page d'accueil

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
-import { latLng, tileLayer } from 'leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-contact',
@@ -12,12 +12,42 @@ import { latLng, tileLayer } from 'leaflet';
 export class ContactComponent {
   options = {
     layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 30,
         attribution: '...',
       }),
     ],
     zoom: 7,
-    center: latLng(-22.272453946632073, 166.43851442916946),
+    center: L.latLng(-22.280849,  166.433937),
   };
+  layersControl = {
+    baseLayers: {
+      'Open Street Map': L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+      'Open Cycle Map': L.tileLayer('https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    },
+    overlays: {
+      'Big Circle': L.circle([ 46.95, -122 ], { radius: 5000 }),
+      'Big Square': L.polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
+    }
+  }
+  layers = [
+    L.marker([ -22.280849, 166.433937], {
+      icon: L.icon({
+        iconUrl: '../../../assets/marker-icon.png',
+        shadowUrl: '../../../assets/marker-shadow.png',
+      })
+    }).on('click', event => {
+      console.log('Yay, my marker was clicked!', event);
+    }),
+  ];
+  
+  onMarkerClick(e: any) {
+    console.log('Clic sur le marqueur :', e);
+  }
 }
+
+// coordonnées lcvck
+// -22.280849, 166.433937
+
+//   polygon([[ -22.280849, 166.433937 ], [ 46.92, -121.92 ], [ 46.87, -121.8 ]]),
+
