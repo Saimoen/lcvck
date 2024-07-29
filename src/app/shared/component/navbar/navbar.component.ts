8e5d517a81776aa0f9eb1bb7d8a0bb640dc7c187
Saimoen <gregorysaimoen@outlook.com>
2024-07-29 20:01:57 +1100

    ui amélioré

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, NgIf, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  user: firebase.User | null = null;
 
+
+  constructor(
+    private userService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit() { 
+    this.userService.user$.subscribe((user) => {
+      this.user = user;
+      console.log('User:', user);
+    });
+  }
+
+  logout() {
+    this.userService.afAuth.signOut();
+    this.router.navigateByUrl('/accueil');
+  }
 }
