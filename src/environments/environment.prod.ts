1d1037a409cb81eac6c27d6ef511f01fd7d5db20
Gregory SAIMOEN <gregory.saimoen@gouv.nc>
2024-10-23 07:38:07 +1100

    Prepare app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,4 @@ export const environment = {
     production: true,
     apiUrl: 'https://lcvck-back-d4ece89d2d6b.herokuapp.com/'
 }
+// Compare this snippet from src/environments/environment.prod.ts:
